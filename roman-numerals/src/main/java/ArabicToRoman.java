2b19761e6ab077be87181b5b748e21d129bd73fa
Klaas De Cock <klaas.decock@cm.be>
2023-06-18 18:40:43 +0200

    improve ArabicToRoman

--- roman-numerals/src/main/java/ArabicToRoman.java
+++ roman-numerals/src/main/java/ArabicToRoman.java
@@ -8,47 +8,38 @@ public class ArabicToRoman {
 
     public static void main(String[] args) {
 
-        String input = "1999";
+        String input = "1929";
         StringBuilder output = new StringBuilder();
 
-        List<Integer> inputList = convertInputToIntegerList(input);
+        List<Integer> inputList = convertInputToList(input);
+
         validateInput(inputList);
 
+        calculateRomanNumeral(output, inputList);
+
+        System.out.println("arabic number " + input + " converted to roman number " + output);
+    }
+
+    private static void calculateRomanNumeral(StringBuilder output, List<Integer> inputList) {
         while (inputList.size() != 0) {
-            inputList = calculateRomanNumeral(output, inputList, inputList.size() - 1);
+            int index = inputList.size() - 1;
+            String firstRomanDigit = romanNumerals.get(2 * index);
+            String secondRomanDigit = romanNumerals.get(2 * index + 1);
+            String thirdRomanDigit = romanNumerals.get(2 * index + 2);
+            Integer consideredNumber = inputList.get(0);
+            if (consideredNumber / 9 == 1) {
+                output.append(firstRomanDigit);
+                output.append(thirdRomanDigit);
+            } else if (consideredNumber / 5 == 1) {
+                output.append(secondRomanDigit);
+                output.append(firstRomanDigit.repeat(Math.max(0, consideredNumber - 5)));
+            } else if (consideredNumber / 4 == 1) {
+                output.append(firstRomanDigit).append(secondRomanDigit);
+            } else {
+                output.append(firstRomanDigit.repeat(Math.max(0, consideredNumber)));
+            }
+            inputList.remove(inputList.get(0));
         }
-
-        System.out.println(output);
-
-    }
-
-    private static List<Integer> calculateRomanNumeral(StringBuilder output, List<Integer> inputList, int indexNumber) {
-        String firstRomanDigit = romanNumerals.get(2*indexNumber);
-        String secondRomanDigit = romanNumerals.get(2*indexNumber + 1);
-        String thirdRomanDigit = romanNumerals.get(2*indexNumber + 2);
-        Integer consideredNumber = inputList.get(0);
-        if (consideredNumber / 9 == 1) {
-            output.append(firstRomanDigit);
-            output.append(thirdRomanDigit);
-        } else if (consideredNumber / 5 == 1) {
-            output.append(secondRomanDigit);
-            output.append(firstRomanDigit.repeat(Math.max(0, consideredNumber - 5)));
-        } else if (consideredNumber / 4 == 1) {
-            output.append(firstRomanDigit).append(secondRomanDigit);
-        } else {
-            output.append(firstRomanDigit.repeat(Math.max(0, consideredNumber)));
-        }
-        inputList = removeConsideredNumberFromInputList(inputList);
-        return inputList;
-    }
-
-    private static List<Integer> removeConsideredNumberFromInputList(List<Integer> inputList) {
-        List<Integer> newInputList = new ArrayList<>();
-        for (int i = 1; i < inputList.size(); i++) {
-            newInputList.add(inputList.get(i));
-        }
-        inputList = newInputList;
-        return inputList;
     }
 
 
@@ -64,16 +55,15 @@ public class ArabicToRoman {
         }
     }
 
-    private static List<Integer> convertInputToIntegerList(String input) {
+    private static List<Integer> convertInputToList(String input) {
         List<Integer> inputList;
         try {
-            inputList = Arrays.stream(input.split(""))
+            inputList = new ArrayList<>(Arrays.stream(input.split(""))
                     .map(Integer::parseInt)
-                    .toList();
+                    .toList());
         } catch (NumberFormatException e) {
             throw new InvalidNumberException("input string can only contain numerals");
         }
-        System.out.println(inputList);
         return inputList;
     }
 
