e67a4dd5a7f4b715088e595ee5b63043bb47a1ab
Klaas De Cock <klaas.decock@cm.be>
2023-07-03 22:12:55 +0200

    add method parseInput with unit test

--- bowling/src/main/java/BowlingService.java
+++ bowling/src/main/java/BowlingService.java
@@ -1,2 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class BowlingService {
+
+    public List<Integer> parseInput(String input) {
+        List<Integer> pinsDown = new ArrayList<>();
+        for (String scorePerFrame : input.split(" ")) {
+            if ("X".equals(scorePerFrame)) {
+                pinsDown.add(10);
+            } else {
+                if (scorePerFrame.length() == 2) {
+                    String[] rollsPerFrame = scorePerFrame.split("");
+                    int roll1 = Integer.parseInt(rollsPerFrame[0]);
+                    pinsDown.add(roll1);
+                    String roll2 = rollsPerFrame[1];
+                    if ("-".equals(roll2)) {
+                        pinsDown.add(0);
+                    } else if ("/".equals(roll2)) {
+                        pinsDown.add(10 - roll1);
+                    } else {
+                        pinsDown.add(Integer.parseInt(roll2));
+                    }
+                } else {
+                    pinsDown.add(Integer.parseInt(scorePerFrame));
+                }
+            }
+        }
+        return pinsDown;
+    }
+
 }
