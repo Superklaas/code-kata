fcdefde2d38f1dc2fef095f464f18049c694c323
Klaas De Cock <klaas.decock@cm.be>
2023-07-05 22:10:06 +0200

    format code

--- bowling/src/main/java/com/superklaas/BowlingService.java
+++ bowling/src/main/java/com/superklaas/BowlingService.java
@@ -10,16 +10,16 @@ public class BowlingService {
         int rollIndex = 1;
         for (String scorePerFrame : input.split(" ")) {
             if ("X".equals(scorePerFrame)) {
-                rollList.add(new Roll(rollIndex,10));
+                rollList.add(new Roll(rollIndex, 10));
             } else {
                 if (scorePerFrame.length() == 2) {
                     String[] rollsPerFrame = scorePerFrame.split("");
                     int pinsRoll1 = Integer.parseInt(rollsPerFrame[0]);
-                    rollList.add(new Roll(rollIndex,pinsRoll1));
+                    rollList.add(new Roll(rollIndex, pinsRoll1));
                     rollIndex++;
                     String roll2 = rollsPerFrame[1];
                     if ("-".equals(roll2)) {
-                        rollList.add(new Roll(rollIndex,0));
+                        rollList.add(new Roll(rollIndex, 0));
                     } else if ("/".equals(roll2)) {
                         rollList.add(new Roll(rollIndex, 10 - pinsRoll1));
                     } else {
