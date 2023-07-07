e8ef10dad1ac50d45c86803ec042522f04155d4c
Klaas De Cock <klaas.decock@cm.be>
2023-07-07 22:50:25 +0200

    convert model classes to records with implemented toString method

--- bowling/src/main/java/com/superklaas/Frame.java
+++ bowling/src/main/java/com/superklaas/Frame.java
@@ -1,38 +1,16 @@
 package com.superklaas;
 
-public class Frame {
+public record Frame(int frameIndex, Roll roll1, Roll roll2, boolean isSpare, boolean isStrike) {
 
-    private final int frameIndex;
-    private final Roll roll1;
-    private final Roll roll2;
-    private final boolean isSpare;
-    private final boolean isStrike;
-
-    public Frame(int frameIndex, Roll roll1, Roll roll2, boolean isSpare, boolean isStrike) {
-        this.frameIndex = frameIndex;
-        this.roll1 = roll1;
-        this.roll2 = roll2;
-        this.isSpare = isSpare;
-        this.isStrike = isStrike;
+    @Override
+    public String toString() {
+        return "Frame{" +
+                "frameIndex=" + frameIndex +
+                ", roll1=" + roll1 +
+                ", roll2=" + roll2 +
+                ", isSpare=" + isSpare +
+                ", isStrike=" + isStrike +
+                '}';
     }
 
-    public int getFrameIndex() {
-        return frameIndex;
-    }
-
-    public Roll getRoll1() {
-        return roll1;
-    }
-
-    public Roll getRoll2() {
-        return roll2;
-    }
-
-    public boolean isSpare() {
-        return isSpare;
-    }
-
-    public boolean isStrike() {
-        return isStrike;
-    }
 }
--- bowling/src/main/java/com/superklaas/Roll.java
+++ bowling/src/main/java/com/superklaas/Roll.java
@@ -1,25 +1,10 @@
 package com.superklaas;
 
-public class Roll {
-
-    private final int rollIndex;
-    private final int pins;
-
-    public Roll(int rollIndex, int pins) {
-        this.rollIndex = rollIndex;
-        this.pins = pins;
-    }
-
-    public int getRollIndex() {
-        return rollIndex;
-    }
-
-    public int getPins() {
-        return pins;
-    }
+public record Roll(int rollIndex, int pins) {
 
     @Override
     public String toString() {
         return "{" + rollIndex + "," + pins + "}";
     }
+
 }
