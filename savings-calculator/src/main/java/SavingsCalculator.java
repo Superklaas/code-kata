13b40077a4dbe25c6550a6d806930e8b018f2a84
Klaas Decock <klaasdc@icloud.com>
2024-08-17 16:46:34 +0200

    refactoring savings calculator

--- savings-calculator/src/main/java/SavingsCalculator.java
+++ savings-calculator/src/main/java/SavingsCalculator.java
@@ -2,15 +2,15 @@ public class SavingsCalculator {
 
     public static void main(String[] args) {
 
-        double yearlySavings = 20000;
+        double yearlySavings = 1000;
         double growthFactor = 1.021;
         int years = 10;
 
-        double totalGrowthFactor2 = 0;
-        for (int i2 = 1; i2 <= years; i2++) {
-            totalGrowthFactor2 += Math.pow(growthFactor, i2);
+        double totalGrowthFactor = 0;
+        for (int i = 1; i <= years; i++) {
+            totalGrowthFactor += Math.pow(growthFactor, i);
         }
-        double totalSavings = yearlySavings * totalGrowthFactor2;
+        double totalSavings = yearlySavings * totalGrowthFactor;
         System.out.printf("Total savings amount: € %,.2f%n", totalSavings);
 
         double totalProfit = totalSavings - yearlySavings * years;
@@ -20,17 +20,5 @@ public class SavingsCalculator {
         System.out.printf("Total interest rate: %.2f %%%n", totalInterestRate);
 
     }
-
-
-    //        Scanner scanner = new Scanner(System.in);
-//
-//        System.out.println("Enter yearly savings: ");
-//        double yearlySavings = scanner.nextDouble();
-//
-//        System.out.println("Enter interest rate: ");
-//        double growthFactor = 1 + scanner.nextDouble()/100;
-//
-//        System.out.println("Enter amount of years: ");
-//        int years = scanner.nextInt();
 
 }
