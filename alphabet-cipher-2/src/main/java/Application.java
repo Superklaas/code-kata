8ad2924b1e4b129f6ff5b921055a7758f6387fe2
Klaas Decock <klaasdc@icloud.com>
2024-12-29 21:04:37 +1300

    put cipher logic into CipherService

--- alphabet-cipher-2/src/main/java/Application.java
+++ alphabet-cipher-2/src/main/java/Application.java
@@ -2,51 +2,15 @@ public class Application {
 
     public static void main(String[] args) {
 
+        CipherService cipherService = new CipherService();
+
         String input = "cometothepantryofthegrandsalon";
         String keyword = "scones";
-        String output = applyAlphabetCipher(input, keyword, CypherDirection.ENCODE);
-        System.out.println(output);
+        String output = cipherService.applyAlphabetCipher(input, keyword, CypherDirection.ENCODE);
+        System.out.printf("String %s encoded to %s%n", input, output);
 
-        String originalInput = applyAlphabetCipher(output, keyword, CypherDirection.DECODE);
-        System.out.println(originalInput);
-
-    }
-
-    private static String applyAlphabetCipher(String input, String keyword, CypherDirection cypherDirection) {
-
-        input = input.toUpperCase();
-        keyword = keyword.toUpperCase();
-        StringBuilder output = new StringBuilder();
-
-        for (int i = 0; i < input.length(); i++) {
-
-            int inputIndex = Alphabet.valueOf(String.valueOf(input.charAt(i))).ordinal();
-
-            int keywordIndex;
-            if (i >= keyword.length()) {
-                keywordIndex = Alphabet.valueOf(String.valueOf(keyword.charAt(i % keyword.length()))).ordinal();
-            } else {
-                keywordIndex = Alphabet.valueOf(String.valueOf(keyword.charAt(i))).ordinal();
-            }
-
-            int outputIndex;
-            if (CypherDirection.ENCODE == cypherDirection) {
-                outputIndex = inputIndex + keywordIndex;
-                if (outputIndex > 25) {
-                    outputIndex = outputIndex - 26;
-                }
-            } else {
-                outputIndex = inputIndex - keywordIndex;
-                if (outputIndex < 0) {
-                    outputIndex = outputIndex + 26;
-                }
-            }
-
-            output.append(Alphabet.values()[outputIndex].name());
-
-        }
-
-        return output.toString().toLowerCase();
+        String originalInput = cipherService.applyAlphabetCipher(output, keyword, CypherDirection.DECODE);
+        System.out.printf("String %s decoded to %s%n", output, originalInput);
 
     }
 
---
+++ alphabet-cipher-2/src/main/java/CipherService.java
@@ -0,0 +1,41 @@
+public class CipherService {
+
+
+    public String applyAlphabetCipher(String input, String keyword, CypherDirection cypherDirection) {
+
+        input = input.toUpperCase();
+        keyword = keyword.toUpperCase();
+        StringBuilder output = new StringBuilder();
+
+        for (int i = 0; i < input.length(); i++) {
+
+            int inputIndex = Alphabet.valueOf(String.valueOf(input.charAt(i))).ordinal();
+
+            int keywordIndex;
+            if (i >= keyword.length()) {
+                keywordIndex = Alphabet.valueOf(String.valueOf(keyword.charAt(i % keyword.length()))).ordinal();
+            } else {
+                keywordIndex = Alphabet.valueOf(String.valueOf(keyword.charAt(i))).ordinal();
+            }
+
+            int outputIndex;
+            if (CypherDirection.ENCODE == cypherDirection) {
+                outputIndex = inputIndex + keywordIndex;
+                if (outputIndex > 25) {
+                    outputIndex = outputIndex - 26;
+                }
+            } else {
+                outputIndex = inputIndex - keywordIndex;
+                if (outputIndex < 0) {
+                    outputIndex = outputIndex + 26;
+                }
+            }
+
+            output.append(Alphabet.values()[outputIndex].name());
+
+        }
+
+        return output.toString().toLowerCase();
+
+    }
+}
