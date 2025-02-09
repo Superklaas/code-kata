baede7ff878a843971c4e47073f62a1fb997103f
Klaas Decock <klaasdc@icloud.com>
2025-02-09 21:20:42 +0100

    aoc 2024 day 4: refactor part 2

--- advent-of-code-2024/day-4/src/main/java/Part2.java
+++ advent-of-code-2024/day-4/src/main/java/Part2.java
@@ -2,69 +2,36 @@ import java.io.IOException;
 
 public class Part2 {
 
+    // four possible crosses: MasMas, MasSam, SamMas, SamSam
+    private static final String[][] letterCombinations = {
+            {"M", "M", "S", "S"},
+            {"M", "S", "M", "S"},
+            {"S", "M", "S", "M"},
+            {"S", "S", "M", "M"},
+    };
+    private static int counter;
+
     public static void main(String[] args) throws IOException {
-
         String[][] inputArray = Commons.convertInputTo2DArray();
-
-        int countMas = 0;
         for (int x = 0; x < inputArray.length; x++) {
             for (int y = 0; y < inputArray[x].length; y++) {
-                if ("M".equals(inputArray[x][y])) {
-                    countMas = checkMasMas(inputArray, countMas, x, y);
-                    countMas = checkMasSam(inputArray, countMas, x, y);
-                }
-                if ("S".equals(inputArray[x][y])) {
-                    countMas = checkSamMas(inputArray, countMas, x, y);
-                    countMas = checkSamSam(inputArray, countMas, x, y);
+                for (String[] letterCombination : letterCombinations) {
+                    counter += checkMas(inputArray, x, y, letterCombination) ? 1 : 0;
                 }
             }
         }
-        System.out.println(countMas);
+        System.out.println(counter);
 
     }
 
-    private static int checkSamSam(String[][] inputArray, int count, int x, int y) {
-        if (x + 2 < inputArray.length && y + 2 < inputArray[x].length &&
-                "S".equals(inputArray[x + 2][y]) &&
+    private static boolean checkMas(String[][] inputArray, int x, int y, String[] letterCombination) {
+        return x + 2 < inputArray.length &&
+                y + 2 < inputArray[x].length &&
                 "A".equals(inputArray[x + 1][y + 1]) &&
-                "M".equals(inputArray[x][y + 2]) &&
-                "M".equals(inputArray[x + 2][y + 2])) {
-            count++;
-        }
-        return count;
-    }
-
-    private static int checkSamMas(String[][] inputArray, int count, int x, int y) {
-        if (x + 2 < inputArray.length && y + 2 < inputArray[x].length &&
-                "M".equals(inputArray[x + 2][y]) &&
-                "A".equals(inputArray[x + 1][y + 1]) &&
-                "S".equals(inputArray[x][y + 2]) &&
-                "M".equals(inputArray[x + 2][y + 2])) {
-            count++;
-        }
-        return count;
-    }
-
-    private static int checkMasSam(String[][] inputArray, int count, int x, int y) {
-        if (x + 2 < inputArray.length && y + 2 < inputArray[x].length &&
-                "S".equals(inputArray[x + 2][y]) &&
-                "A".equals(inputArray[x + 1][y + 1]) &&
-                "M".equals(inputArray[x][y + 2]) &&
-                "S".equals(inputArray[x + 2][y + 2])) {
-            count++;
-        }
-        return count;
-    }
-
-    private static int checkMasMas(String[][] inputArray, int count, int x, int y) {
-        if (x + 2 < inputArray.length && y + 2 < inputArray[x].length &&
-                "M".equals(inputArray[x + 2][y]) &&
-                "A".equals(inputArray[x + 1][y + 1]) &&
-                "S".equals(inputArray[x][y + 2]) &&
-                "S".equals(inputArray[x + 2][y + 2])) {
-            count++;
-        }
-        return count;
+                letterCombination[0].equals(inputArray[x][y]) &&
+                letterCombination[1].equals(inputArray[x][y + 2]) &&
+                letterCombination[2].equals(inputArray[x + 2][y]) &&
+                letterCombination[3].equals(inputArray[x + 2][y + 2]);
     }
 
 }
