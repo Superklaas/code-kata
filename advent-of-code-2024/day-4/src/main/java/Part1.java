f238a86ab76b9b3dd9c51c89a106fdd586ea0dbe
Klaas Decock <klaasdc@icloud.com>
2025-02-09 22:16:14 +0100

    aoc 2024 day 4: refactor part 1

--- advent-of-code-2024/day-4/src/main/java/Part1.java
+++ advent-of-code-2024/day-4/src/main/java/Part1.java
@@ -2,108 +2,55 @@ import java.io.IOException;
 
 public class Part1 {
 
+    static final int[][] directions = {
+            {-1, 0},    // north
+            {-1, 1},    // northeast
+            {0, 1},     // east
+            {1, 1},     // southeast
+            {1, 0},     // south
+            {1, -1},    // southwest
+            {0, -1},    // west
+            {-1, -1}    // northwest
+    };
+
+    private static int counter;
+
     public static void main(String[] args) throws IOException {
 
         String[][] inputArray = Commons.convertInputTo2DArray();
 
-        int countX = 0;
         for (int x = 0; x < inputArray.length; x++) {
             for (int y = 0; y < inputArray[x].length; y++) {
                 if ("X".equals(inputArray[x][y])) {
-                    countX = checkThreeOClock(inputArray, countX, x, y);
-                    countX = checkNineOClock(inputArray, countX, x, y);
-                    countX = checkTwelveOClock(inputArray, countX, x, y);
-                    countX = checkSixOClock(inputArray, countX, x, y);
-                    countX = checkTenThirty(inputArray, countX, x, y);
-                    countX = checkOneThirty(inputArray, countX, x, y);
-                    countX = checkFourThirty(inputArray, countX, x, y);
-                    countX = checkSevenThirty(inputArray, countX, x, y);
+                    for (int[] direction : directions) {
+                        counter += checkDirection(inputArray, x, y, direction) ? 1 : 0;
+                    }
                 }
             }
         }
 
-        System.out.println(countX);
+        System.out.println(counter);
 
     }
 
-    private static int checkSevenThirty(String[][] inputArray, int count, int x, int y) {
-        if (x - 3 > -1 && y + 3 < inputArray[x].length &&
-                "M".equals(inputArray[x - 1][y + 1]) &&
-                "A".equals(inputArray[x - 2][y + 2]) &&
-                "S".equals(inputArray[x - 3][y + 3])) {
-            count++;
-        }
-        return count;
+    private static boolean checkDirection(String[][] inputArray, int x, int y, int[] direction) {
+        int dx = direction[0];
+        int dy = direction[1];
+        if (isOutsideGrid(inputArray.length, x, y, dx, dy)) return false;
+        return isMatchingCharacters(inputArray, x, y, dx, dy);
     }
 
-    private static int checkFourThirty(String[][] inputArray, int count, int x, int y) {
-        if (x + 3 < inputArray.length && y + 3 < inputArray[x].length &&
-                "M".equals(inputArray[x + 1][y + 1]) &&
-                "A".equals(inputArray[x + 2][y + 2]) &&
-                "S".equals(inputArray[x + 3][y + 3])) {
-            count++;
-        }
-        return count;
+    private static boolean isOutsideGrid(int arrayLength, int x, int y, int dx, int dy) {
+        return (dx == 1 && x + 3 >= arrayLength) ||
+                (dx == -1 && x - 3 < 0) ||
+                (dy == 1 && y + 3 >= arrayLength) ||
+                (dy == -1 && y - 3 < 0);
     }
 
-    private static int checkOneThirty(String[][] inputArray, int count, int x, int y) {
-        if (x + 3 < inputArray.length && y - 3 > -1 &&
-                "M".equals(inputArray[x + 1][y - 1]) &&
-                "A".equals(inputArray[x + 2][y - 2]) &&
-                "S".equals(inputArray[x + 3][y - 3])) {
-            count++;
-        }
-        return count;
-    }
-
-    private static int checkTenThirty(String[][] inputArray, int count, int x, int y) {
-        if (x - 3 > -1 && y - 3 > -1 &&
-                "M".equals(inputArray[x - 1][y - 1]) &&
-                "A".equals(inputArray[x - 2][y - 2]) &&
-                "S".equals(inputArray[x - 3][y - 3])) {
-            count++;
-        }
-        return count;
-    }
-
-    private static int checkSixOClock(String[][] inputArray, int count, int x, int y) {
-        if (x + 3 < inputArray.length &&
-                "M".equals(inputArray[x + 1][y]) &&
-                "A".equals(inputArray[x + 2][y]) &&
-                "S".equals(inputArray[x + 3][y])) {
-            count++;
-        }
-        return count;
-    }
-
-    private static int checkTwelveOClock(String[][] inputArray, int count, int x, int y) {
-        if (x - 3 > -1 &&
-                "M".equals(inputArray[x - 1][y]) &&
-                "A".equals(inputArray[x - 2][y]) &&
-                "S".equals(inputArray[x - 3][y])) {
-            count++;
-        }
-        return count;
-    }
-
-    private static int checkNineOClock(String[][] inputArray, int count, int x, int y) {
-        if (y - 3 > -1 &&
-                "M".equals(inputArray[x][y - 1]) &&
-                "A".equals(inputArray[x][y - 2]) &&
-                "S".equals(inputArray[x][y - 3])) {
-            count++;
-        }
-        return count;
-    }
-
-    private static int checkThreeOClock(String[][] inputArray, int count, int x, int y) {
-        if (y + 3 < inputArray[x].length &&
-                "M".equals(inputArray[x][y + 1]) &&
-                "A".equals(inputArray[x][y + 2]) &&
-                "S".equals(inputArray[x][y + 3])) {
-            count++;
-        }
-        return count;
+    private static boolean isMatchingCharacters(String[][] inputArray, int x, int y, int dx, int dy) {
+        return "M".equals(inputArray[x + dx][y + dy]) &&
+                "A".equals(inputArray[x + 2 * dx][y + 2 * dy]) &&
+                "S".equals(inputArray[x + 3 * dx][y + 3 * dy]);
     }
 
 }
