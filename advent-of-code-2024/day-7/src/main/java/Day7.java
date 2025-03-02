f80c8c73ed7759b8dcc311e4dc9be4965c0ce9f5
Klaas Decock <klaasdc@icloud.com>
2025-03-02 21:41:07 +0100

    aoc 2024 day 7 part 1

---
+++ advent-of-code-2024/day-7/src/main/java/Day7.java
@@ -0,0 +1,70 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Day7 {
+
+    public static final String INPUT = "advent-of-code-2024/day-7/input/input.txt";
+    public static final String TEST_INPUT = "advent-of-code-2024/day-7/input/test-input.txt";
+
+    private List<Long> allResults;
+    private List<List<Long>> allOperands;
+
+    public static void main(String[] args) throws IOException {
+        Day7 day7 = new Day7();
+        day7.parseInput();
+        // PART 1
+        long sumValidResults = day7.calculateSumValidResults();
+        System.out.println(sumValidResults);
+
+    }
+
+    private void parseInput() throws IOException {
+        List<String[]> resultsWithOperands = Files.readAllLines(Path.of(TEST_INPUT)).stream()
+                .map(inputLine -> inputLine.split(":", 2))
+                .filter(array -> array.length == 2)
+                .toList();
+        allResults = resultsWithOperands.stream()
+                .map(array -> array[0].trim())
+                .map(Long::parseLong)
+                .toList();
+        allOperands = resultsWithOperands.stream()
+                .map(array -> Arrays.stream(array[1].trim().split(" "))
+                        .map(Long::parseLong)
+                        .toList())
+                .toList();
+    }
+
+    private long calculateSumValidResults() {
+        long sumValidResults = 0L;
+        for (int i = 0; i < allResults.size(); i++) {
+            Long actualResult = allResults.get(i);
+            List<Long> operands = allOperands.get(i);
+            Set<Long> possibleResults = computePossibleResults(operands);
+            if (possibleResults.contains(actualResult)) sumValidResults += actualResult;
+        }
+        return sumValidResults;
+    }
+
+    public static Set<Long> computePossibleResults(List<Long> operands) {
+        Set<Long> possibleResults = new HashSet<>();
+        if (operands == null || operands.isEmpty()) return possibleResults;
+        compute(operands, 1, operands.get(0), possibleResults);
+        return possibleResults;
+    }
+
+    private static void compute(List<Long> operands, int index, long currentValue, Set<Long> possibleResults) {
+        if (index == operands.size()) {
+            possibleResults.add(currentValue);
+            return;
+        }
+        long nextNum = operands.get(index);
+        compute(operands, index + 1, currentValue + nextNum, possibleResults);
+        compute(operands, index + 1, currentValue * nextNum, possibleResults);
+    }
+
+}
