367ab2f7da8925bb6180eb8afb24966d3b43c5d2
Klaas Decock <klaasdc@icloud.com>
2025-03-02 21:59:55 +0100

    aoc 2024 day 7 part 2

--- advent-of-code-2024/day-7/src/main/java/Day7.java
+++ advent-of-code-2024/day-7/src/main/java/Day7.java
@@ -17,10 +17,12 @@ public class Day7 {
     public static void main(String[] args) throws IOException {
         Day7 day7 = new Day7();
         day7.parseInput();
-        // PART 1
-        long sumValidResults = day7.calculateSumValidResults();
+        // PART 1: addition & multiplication
+        long sumValidResults = day7.calculateSumValidResults(false);
         System.out.println(sumValidResults);
-
+        // PART 2: cf. part 1 + concatenation
+        sumValidResults = day7.calculateSumValidResults(true);
+        System.out.println(sumValidResults);
     }
 
     private void parseInput() throws IOException {
@@ -39,32 +41,35 @@ public class Day7 {
                 .toList();
     }
 
-    private long calculateSumValidResults() {
+    private long calculateSumValidResults(boolean withConcat) {
         long sumValidResults = 0L;
         for (int i = 0; i < allResults.size(); i++) {
             Long actualResult = allResults.get(i);
             List<Long> operands = allOperands.get(i);
-            Set<Long> possibleResults = computePossibleResults(operands);
+            Set<Long> possibleResults = computePossibleResults(operands, withConcat);
             if (possibleResults.contains(actualResult)) sumValidResults += actualResult;
         }
         return sumValidResults;
     }
 
-    public static Set<Long> computePossibleResults(List<Long> operands) {
+    public static Set<Long> computePossibleResults(List<Long> operands, boolean withConcat) {
         Set<Long> possibleResults = new HashSet<>();
         if (operands == null || operands.isEmpty()) return possibleResults;
-        compute(operands, 1, operands.get(0), possibleResults);
+        compute(operands, 1, operands.get(0), possibleResults, withConcat);
         return possibleResults;
     }
 
-    private static void compute(List<Long> operands, int index, long currentValue, Set<Long> possibleResults) {
-        if (index == operands.size()) {
+    private static void compute(List<Long> operands, int nextIndex, long currentValue, Set<Long> possibleResults, boolean withConcat) {
+        if (nextIndex == operands.size()) {
             possibleResults.add(currentValue);
             return;
         }
-        long nextNum = operands.get(index);
-        compute(operands, index + 1, currentValue + nextNum, possibleResults);
-        compute(operands, index + 1, currentValue * nextNum, possibleResults);
+        long nextValue = operands.get(nextIndex);
+        compute(operands, nextIndex + 1, currentValue + nextValue, possibleResults, withConcat);
+        compute(operands, nextIndex + 1, currentValue * nextValue, possibleResults, withConcat);
+        if (withConcat) {
+            compute(operands, nextIndex + 1, Long.parseLong(currentValue + "" + nextValue), possibleResults, true);
+        }
     }
 
 }
